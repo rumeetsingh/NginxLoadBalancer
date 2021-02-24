efa1dfd0811cecab2185f57946b337f2dcb9eaeb
Rumeet Singh <singh.rumeet5@gmail.com>
2021-02-24 17:30:13 +0530

    first commit

---
+++ Server02/src/_next/static/OkHXLDyzlNwxc16IESFBy/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fchunks\u002Fpages\u002Findex-93246daee5e6ee5b8b57.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-b328101be46c15f89243.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
